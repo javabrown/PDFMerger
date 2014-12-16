36ceb42c0d866e93c11710378cd952ae1d9afa17
javabrown <getrk@yahoo.com>
2014-12-16 00:13:41 -0500

    Added remove pdf functionality.

--- src/com/javabrown/pdf/BoxFrame.java
+++ src/com/javabrown/pdf/BoxFrame.java
@@ -1,11 +1,16 @@
 package com.javabrown.pdf;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.UIManager;
 
 public class BoxFrame extends JFrame {
@@ -13,12 +18,41 @@ public class BoxFrame extends JFrame {
 		this.setTitle("JavaBrown - PDF Merge");
 		this.setOSLookAndFeel();
 		this.setSize(300, 300);
+		this.setMinimumSize(new Dimension(300, 300));
 		this.setDefaultCloseOperation(3);
 		this.getContentPane().setLayout(new BorderLayout(10, 10));
 		this.getContentPane().add(new Dashboard(), BorderLayout.CENTER);
+		//this.setLayeredPane(getBackgroundLayer());
 		this.setVisible(true);
 	}
 
+	public JLayeredPane getBackgroundLayer() {
+		String IMAGE_SITE = "http://internetofeverything.cisco.com/sites/"
+				+ "default/files/styles/image_text_tile/public/"
+				+ "link_tiles/Marquee_images.png";
+
+		JLayeredPane layeredPanel = new JLayeredPane();
+
+		//layeredPanel.setPreferredSize(new Dimension(200, 200));
+		try {
+			URL url = new URL(IMAGE_SITE);
+			BufferedImage image = ImageIO.read(url);
+			ImagePanel imagePanel = new ImagePanel(image);
+			imagePanel.setSize(layeredPanel.getPreferredSize());
+
+			//layeredPanel.add(imagePanel, JLayeredPane.DEFAULT_LAYER);
+			layeredPanel.add(imagePanel, JLayeredPane.PALETTE_LAYER);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		return layeredPanel;
+	}
+	
 	private void setOSLookAndFeel() {
 		NativeData nativeInfo = new NativeData();
 		String OS = System.getProperty("os.name").toLowerCase();
--- src/com/javabrown/pdf/Dashboard.java
+++ src/com/javabrown/pdf/Dashboard.java
@@ -1,16 +1,24 @@
 package com.javabrown.pdf;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -18,94 +26,122 @@ import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
 
 public class Dashboard extends JPanel {
-	  private PDFMerger _pdfMerger;
+	private PDFMerger _pdfMerger;
 
-	  private JList _jList;
-	  private DefaultListModel _listModel;
+	private JList _jList;
+	private DefaultListModel _listModel;
 
-	  private JButton _addPdfFileBtn;
-	  private JButton _joinBtn;
-	  private JButton _resetBtn;
-	  private JPanel _controlPanel;
+	private JButton _addPdfFileBtn;
+	private JButton _removePdfFileBtn;
+	private JButton _joinBtn;
+	private JButton _resetBtn;
+	private JPanel _controlPanel;
 
-	  public Dashboard() {
-	    _pdfMerger = new PDFMerger();
+	public Dashboard() {
+		_pdfMerger = new PDFMerger();
 
-	    _listModel = new DefaultListModel();
-	    _jList = new JList(_listModel);
+		_listModel = new DefaultListModel();
+		_jList = new JList(_listModel);
 
-	    _addPdfFileBtn = new JButton("Add PDF File");
-	    _joinBtn = new JButton("Join PDF");
-	    _resetBtn = new JButton("Reset");
+		_addPdfFileBtn = new JButton("Add");
+		_removePdfFileBtn = new JButton("Remove");
+		_joinBtn = new JButton("Join");
+		_resetBtn = new JButton("Reset");
 
-	    super.setLayout(new BorderLayout(10, 10));
-	    super.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
+		super.setLayout(new BorderLayout(10, 10));
+		super.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
 
-
-	    this.pushComponent();
-	    this.registerEvent();
-	  }
-
-	  private final void pushComponent(){
-	    _controlPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
-
-	    this.add(_jList, BorderLayout.CENTER);
-	    this.add(_controlPanel, BorderLayout.SOUTH);
-
-	    _controlPanel.add(_addPdfFileBtn);
-	    _controlPanel.add(_joinBtn);
-	    _controlPanel.add(_resetBtn);
-	  }
-
-	  private final void registerEvent() {
-	    final JFrame frame= (JFrame) SwingUtilities.getWindowAncestor(this);
-
-	    _addPdfFileBtn.addActionListener(new ActionListener() {
-
-	      @Override
-	      public void actionPerformed(ActionEvent arg0) {
-	          JFileChooser openFile = new JFileChooser();
-	          int returnVal = openFile.showOpenDialog(null);
-
-	          if (returnVal == JFileChooser.APPROVE_OPTION) {
-	            File pdfFile = openFile.getSelectedFile();
-	            _pdfMerger.addPdfFile(pdfFile);
-	            _listModel.addElement(pdfFile.getName());
-	          } else {
-	            System.out.println("Open command cancelled by user.");
-	          }
-	      }
-	    });
-
-	    _joinBtn.addActionListener(new ActionListener() {
-	      @Override
-	      public void actionPerformed(ActionEvent arg0) {
-	    	  String workingDir = System.getProperty("user.dir");
-	    	  System.out.println("wok:="+workingDir);
-	        String mergedFileName = _pdfMerger.merge(workingDir);
-
-	         if(mergedFileName != null){
-	           JOptionPane.showMessageDialog(frame,
-	               "Done, merged file:" + mergedFileName);
-	         }
-
-	         try {
-	          Runtime.getRuntime().exec("explorer.exe /select,"+workingDir);
-	          //Runtime.getRuntime().exec("start "+mergedFileName);
-	        } catch (IOException e) {
-	          // TODO Auto-generated catch block
-	          e.printStackTrace();
-	        }
-	      }
-	    });
-
-	    _resetBtn.addActionListener(new ActionListener() {
-	      @Override
-	      public void actionPerformed(ActionEvent arg0) {
-	        _listModel.removeAllElements();
-	        _pdfMerger.reset();
-	      }
-	    });
-
-	  }
+		this.pushComponent();
+		this.registerEvent();
 	}
+
+	private final void pushComponent() {
+		_controlPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+		this.add(_jList, BorderLayout.CENTER);
+		this.add(_controlPanel, BorderLayout.SOUTH);
+
+		_controlPanel.add(_addPdfFileBtn);
+		_controlPanel.add(_removePdfFileBtn);
+		_controlPanel.add(_joinBtn);
+		_controlPanel.add(_resetBtn);
+	}
+
+	private final void registerEvent() {
+		final JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
+
+		_addPdfFileBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser openFile = new JFileChooser();
+				int returnVal = openFile.showOpenDialog(null);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File pdfFile = openFile.getSelectedFile();
+					_pdfMerger.addPdfFile(pdfFile);
+					_listModel.addElement(pdfFile.getName());
+				} else {
+					System.out.println("Open command cancelled by user.");
+				}
+			}
+		});
+
+		_removePdfFileBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int index = _jList.getSelectedIndex();
+				if (index != -1) {
+					_listModel.remove(index);
+				}
+			}
+		});
+
+		_joinBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String workingDir = System.getProperty("user.dir");
+				System.out.println("wok:=" + workingDir);
+				String mergedFileName = _pdfMerger.merge(workingDir);
+
+				if (mergedFileName != null) {
+					JOptionPane.showMessageDialog(frame, "Done, merged file:"
+							+ mergedFileName);
+				}
+
+				try {
+					Runtime.getRuntime().exec(
+							"explorer.exe /select," + workingDir);
+					// Runtime.getRuntime().exec("start "+mergedFileName);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		_resetBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				_listModel.removeAllElements();
+				_pdfMerger.reset();
+			}
+		});
+
+	}
+}
+
+class ImagePanel extends JPanel {
+	private Image image;
+
+	public ImagePanel(Image image) {
+		this.image = image;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (image != null) {
+			g.drawImage(image, 0, 0, null);
+		}
+	}
+}
